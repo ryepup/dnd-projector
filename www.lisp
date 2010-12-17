9763c00c666bc4ddf2fd646c6aa140e3c3ae0e26
Ryan Davis <ryan@mokeys.org>
2010-12-16 22:28:14 -0500

    lots of REPL functions to handle data entry

--- www.lisp
+++ www.lisp
@@ -6,7 +6,7 @@
    hunchentoot:*dispatch-table*))
 
 (defvar *tal-generator* (make-instance 'yaclml:file-system-generator
-		       :cachep T
+		       :cachep nil
 		       :root-directories (list #P"/home/ryan/clbuild/source/dnd-projector/templates/")))
 
 (defun render-tal (tal-file &optional tal-env)
@@ -29,11 +29,17 @@
 (hunchentoot:define-easy-handler (combat-display :uri "/combat-display") ()
   (render-tal "combat-display.tal"))
 
-(hunchentoot:define-easy-handler (scribe :uri "/scribe") ()
-  (unless *current-combat*
+(defun ensure-combat (&optional reset)
+  (when (or reset (null *current-combat*))
     (setf *current-combat* (make-combat))
     (iter (for n in '("Ryepup" "Jack" "Ecthellion" "Tibbar" "Ammonia"))
-	  (add-player n 0 nil *current-combat*)))
+	  (for mod in '(13 10 11 10 17))
+	  (add-player n nil (+ mod (d20)) *current-combat* nil))
+    (sort-players))
+  )
+
+(hunchentoot:define-easy-handler (scribe :uri "/scribe") ()
+  (ensure-combat)
   (render-tal "scribe.tal" (yaclml:tal-env 'players
 				    (iter
 				      (for p in (players *current-combat*))
@@ -58,7 +64,13 @@
 
 (defun sort-players ()
   (setf (players *current-combat*)
-	(sort (players *current-combat*) #'> :key #'initiative)))
+	(sort (players *current-combat*) #'> :key #'initiative)
+
+	(current-init *current-combat*) (iter (for p in (players *current-combat*))
+					      (maximize (initiative p)))
+	)
+  
+  )
 
 (hunchentoot:define-easy-handler (scribe-turn :uri "/scribe-turn") ()
   (turn)
@@ -86,9 +98,7 @@
     (hunchentoot:redirect "/scribe")))
 
 (hunchentoot:define-easy-handler (scribe-kill :uri "/scribe-kill") (id)
-  (setf (players *current-combat*)
-	(remove (parse-integer id) (players *current-combat*)
-		:key #'id))
+  (kill  (parse-integer id))
   (hunchentoot:redirect "/scribe"))
 
 (hunchentoot:define-easy-handler (scribe-save :uri "/scribe-save") (id damage healing new-initiative)
@@ -102,11 +112,14 @@
     (hunchentoot:redirect "/scribe")))
 
 (hunchentoot:define-easy-handler (projector :uri "/projector") ()
-  (render-tal "projector.tal" (yaclml:tal-env 'players
+  (render-tal "projector.tal" (yaclml:tal-env
+			       'current-init (current-init *current-combat*)
+			       'players
 				    (iter
 				      (for p in (players *current-combat*))
 				      (collect (yaclml:tal-env
-					      'name (name p)
+					      'name (name p) 
+					      'pid (id p)
 					      'initiative (initiative p)
 					      'bloody (if (bloodied-p p) "bloody" "")
 					      'css-class (if (hostile-p p) "hostile" "")
@@ -116,11 +129,13 @@
 
 (defclass combat ()
   ((players :accessor players :initform (list))
-   (max-id :accessor max-id :initform 0)))
+   (max-id :accessor max-id :initform 0)
+   (current-init :accessor current-init :initform 0)))
 (defun make-combat () (make-instance 'combat))
-(defmethod player-by-id ((c combat) id)
-  (find (parse-integer id) (players c)
-	:key #'id))
+(defmethod player-by-id ((c combat) (id string))
+  (player-by-id c (parse-integer id)))
+(defmethod player-by-id ((c combat) (id integer))
+  (find id (players c) :key #'id))
 
 (defclass player ()
   ((name :accessor name :initarg :name)
@@ -129,15 +144,6 @@
    (hostile-p :accessor hostile-p :initarg :hostile-p :initform nil)
    (damage :accessor damage :initform 0)
    (id :accessor id :initarg :id)))
-
-(defun add-player (name &optional (hostile-p T) (initiative 0) (combat *current-combat*) (bloodied-p nil))
-  (let ((p (make-instance 'player :name name
-		       :initiative initiative
-		       :hostile-p hostile-p
-		       :bloodied-p bloodied-p
-		       :id (incf (max-id combat)))))
-    (push p (players combat))
-    p))
 
 (defun deserialize-combat-from-string (json)
   (flet ((lookup (key alist) (cdr (assoc key alist))))
@@ -152,6 +158,103 @@
 	  (setf (id pl) (lookup :id p))
 	  ))
       (setf (players c) (nreverse (players c)))
-      c
-      ))
+      c)))
+
+(defun add-player (name &optional (hostile-p T) (initiative 0) (combat *current-combat*) (bloodied-p nil))
+  (let ((p (make-instance 'player :name name
+		       :initiative (typecase initiative
+				     (list (first initiative))
+				     (T initiative))
+		       :hostile-p hostile-p
+		       :bloodied-p bloodied-p
+		       :id (incf (max-id combat)))))
+    (push p (players combat))
+    p))
+
+(defun d20 ()
+  (1+ (random 20)))
+
+(defun add-hostiles (name int-mod &optional (n 1 nsupplied))
+  (if nsupplied
+      (dotimes (i n)
+	(add-hostiles (format nil "~a ~a" name (1+ i)) int-mod))
+      (add-player name T (+ int-mod (d20)))))
+
+(defun kill (&rest ids)
+  (dolist (id ids)
+    (setf (players *current-combat*)
+	  (remove id (players *current-combat*)
+		  :key #'id))))
+
+(defun damagem (amt &rest ids)
+  (dolist (id ids)
+    (incf (damage (player-by-id *current-combat* id))
+	  amt))))
+
+(defun bloodym (bloodyp &rest ids)
+  (dolist (id ids)
+    (setf (bloodied-p (player-by-id *current-combat* id))
+	  bloodyp)))
+
+(defgeneric rename (thing new-name)
+  (:method ((id integer) name)
+    (rename (player-by-id *current-combat* id) name))
+  (:method ((p player) name)
+    (setf (name p) name)))
+
+(defmethod initiative ((id integer))
+  (initiative (player-by-id *current-combat* id)))
+
+(defmethod (setf initiative) (new-init (id integer))
+  (setf (initiative (player-by-id *current-combat* id))
+	new-init
+  ))
+
+(defmethod print-object ((instance player) stream)
+  (print-unreadable-object (instance stream)
+    (with-slots (name id initiative) instance
+      (format stream "~a ~a ~a" name id initiative)))) 
+
+(defun move-up (id)
+  (let* ((p (player-by-id *current-combat* id))
+	 (ps (players *current-combat*))
+	 (ppos (position p ps))
+	 (before (subseq ps 0 ppos))
+	 (after (remove p (subseq ps ppos))))
+ 
+    (if before
+	(let ((l (last before)))
+	  (alexandria:flatten (list (reverse (rest (reverse before)))
+				    p
+				    l after)))
+	(let ((l (last after)))
+	  (alexandria:flatten (list (reverse (rest (reverse after)))
+				    p
+				    l
+				    )))
+	
+	))
+  )
+
+(defun move-down (id)
+  (let* ((p (player-by-id *current-combat* id))
+	 (ps (players *current-combat*))
+	 (ppos (position p ps))
+	 (before (subseq ps 0 ppos))
+	 (after (remove p (subseq ps ppos))))
+ 
+    (if before
+	(let ((l (last before)))
+	  (alexandria:flatten (list
+			       before
+			       (pop after)
+			       p
+			       after)))
+	(let ((l (last after)))
+	  (alexandria:flatten (list (reverse (rest (reverse after)))
+				    p
+				    l
+				    )))
+	
+	))
   )
